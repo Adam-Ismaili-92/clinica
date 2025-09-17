48fe45582ae474f7e71a943a83b54b5e13c2b714
AliceJoubert <alice00joubert@gmail.com>
2025-09-17 10:41:04 +0200

    Adapt interface

--- clinica/pipelines/t1_volume_tissue_segmentation/t1_volume_tissue_segmentation_utils.py
+++ clinica/pipelines/t1_volume_tissue_segmentation/t1_volume_tissue_segmentation_utils.py
@@ -12,6 +12,7 @@ from nipype.interfaces.base import (
     traits,
 )
 from nipype.interfaces.spm.base import SPMCommand, SPMCommandInputSpec
+from nipype.interfaces.spm.preprocess import NewSegment
 from nipype.utils.filemanip import filename_to_list, list_to_filename
 
 
@@ -167,3 +168,14 @@ class ApplySegmentationDeformation(SPMCommand):
             _, fname = os.path.split(filename)
             outputs["out_files"].append(os.path.realpath("w%s" % fname))
         return outputs
+
+
+class ComputeNewSegment(NewSegment):
+    """Use spm_preproc (New Segment) to separate structural images into
+    different tissue classes. Supports multiple modalities.
+
+    Adapts Nipype NewSegment interface to SPM25 and drops SPM8. When Nipype's own interface is adapted this class can be removed.
+    """
+
+    _jobtype = "spatial"
+    _jobname = "preproc"
